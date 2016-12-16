d29fa21a56305a2280d1b2483cc5791dc107d2b2
Ildar Sagdejev <specious@gmail.com>
2016-12-15 19:30:24 -0500

    Bash script to sequentially build a mosaic from a list of Facebook page URLs

---
+++ build-wall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Usage:
+#   facebook-cli likes > likes.txt
+#   awk 'NR % 3 == 2' likes.txt > urls.txt
+#   cat urls.txt | ./buildwall.sh > wall.html
+
+mkdir -p images
+
+while read url; do
+  # Render a dot for every image request
+  (>&2 echo -n ".")
+
+  # Extract page id from page URL
+  id=$(echo $url | cut -d '/' -f 4)
+
+  # Compose URL to retrieve profile picture
+  imgurl="http://graph.facebook.com/$id/picture?type=large"
+
+  # Fetch image (follow redirects)
+  curl -s -L -o "images/$id" $imgurl
+
+  echo "<a href=\"$url\"><img src=\"images/$id\"></a>"
+done < "${1:-/dev/stdin}"
